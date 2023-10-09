23e971a2326932d33ed28ab9a4e8824457bf3a42
Santiago Martín Pérez <smperez@vera.com.uy>
2023-10-09 10:56:41 -0300

    Pruebas con tablas

--- features/parameters.feature
+++ features/parameters.feature
@@ -9,3 +9,14 @@ Feature: Pruebo steps con parametros
      Given que los steps pueden recibir parámetros
       When cuando a un step le pasen un parámetro como este: hola
       Then puedo validar que no coincida con este: hola2
+
+
+  Scenario: Tabla como paraemtros
+     Given Uso una tabla como parametro
+                | key                  | value                  |
+                | ufNaturalidade       | SP                     |
+                | apelido              | apelido.valid          |
+                | declaracaoPpe        | NAO                    |
+                | estadoCivil          | CASADO                 |     
+      When cuando a un step le pasen un parámetro como este: hola
+      Then puedo validar que no coincida con este: hola2
--- features/steps/parameters.py
+++ features/steps/parameters.py
@@ -1,4 +1,18 @@
 from behave import *
+import logging
+
+# Crear un logger
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+# Crear un manejador de archivos que registre incluso los mensajes DEBUG
+fh = logging.FileHandler('my_log.log')
+fh.setLevel(logging.DEBUG)
+# Crear un formateador y añadirlo a los manejadores
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+# Añadir los manejadores al logger
+logger.addHandler(fh)
+
 
 @given('que los steps pueden recibir parámetros')
 def step_impl(context):
@@ -16,3 +30,11 @@ def step_impl(context, parametro):
 @then('puedo validar que no coincida con este: {parametro}')
 def step_impl(context, parametro):
     assert context.param != parametro
+
+@step('Uso una tabla como parametro')
+def step_impl(context):
+    
+    logger.debug("Tabla: " + str(context.table))
+    logger.debug("Tabla: " + context.table.to_string())
+    logger.debug("Tabla - Tipo de dato: " + str(type(context.table)))
+    pass
